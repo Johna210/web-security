3b142b2824f052d8dbe39c3f08e4ca865666731a
Yohannes Solomon <solomonjohna21@gmail.com>
2026-09-11 15:30:27 +0300

    normalize email

--- internal/accounts/store.go
+++ internal/accounts/store.go
@@ -10,6 +10,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"strings"
 	"time"
 
 	"github.com/bootdotdev/learn-web-security/internal/database/dbgen"
@@ -20,7 +21,10 @@ const defaultSessionTTL = 30 * 24 * time.Hour
 var ErrEmailExists = errors.New("an account already exists for that email")
 
 func NormalizeEmail(email string) string {
-	return email
+	newString := strings.TrimSpace(email)
+	newString = strings.ToLower(newString)
+
+	return newString
 }
 
 type User struct {
