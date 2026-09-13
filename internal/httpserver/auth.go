912799c1d0e3a1152024754e456127769d2d5ad0
Yohannes Solomon <solomonjohna21@gmail.com>
2026-09-14 02:16:41 +0300

    sesssion revocation

--- internal/httpserver/auth.go
+++ internal/httpserver/auth.go
@@ -238,6 +238,21 @@ func (handler *authHandler) Logout(responseWriter http.ResponseWriter, request *
 		handler.internalError(responseWriter, request, err)
 		return
 	}
+
+	currentSession, found, err := sessions.Current(request, handler.accounts)
+	if err != nil {
+		handler.internalError(responseWriter, request, err)
+		return
+	}
+
+	if found {
+		err := handler.accounts.RevokeSession(request.Context(), currentSession.Session.Token)
+		if err != nil {
+			handler.internalError(responseWriter, request, err)
+			return
+		}
+	}
+
 	sessions.ClearCookie(responseWriter)
 	if challengeToken != "" {
 		clearTOTPLoginChallengeCookie(responseWriter)
